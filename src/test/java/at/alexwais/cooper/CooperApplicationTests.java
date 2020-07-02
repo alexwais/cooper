d675db2e7e921a53a1dc0c179e4d3e4b72c7138a
Alex Wais <alexwais@users.noreply.github.com>
2020-07-02 21:28:27 +0200

    Add initial project skeleton

---
+++ src/test/java/at/alexwais/cooper/CooperApplicationTests.java
@@ -0,0 +1,13 @@
+package at.alexwais.cooper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
